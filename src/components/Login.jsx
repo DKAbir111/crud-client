61525c5f7bde1762d8a5292e44ef273be250dede
Darun Karas <darunkaras@Daruns-MacBook-Air.local>
2024-11-25 01:08:22 +0600

    connecting with server and send data

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,19 +1,42 @@
 export default function Login() {
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const user = { name, email };
+        console.log(user)
+        fetch('http://localhost:5001/users', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+
+            })
+    }
+
+
     return (
         <div className="flex justify-center items-center mt-20">
             <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-                <form className="card-body">
+                <form className="card-body" onSubmit={handleSubmit}>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Name</span>
+                        </label>
+                        <input type="text" placeholder="Name" name="name" className="input input-bordered" required />
+
+                    </div>
                     <div className="form-control">
                         <label className="label">
                             <span className="label-text">Email</span>
                         </label>
-                        <input type="email" placeholder="email" className="input input-bordered" required />
-                    </div>
-                    <div className="form-control">
-                        <label className="label">
-                            <span className="label-text">Password</span>
-                        </label>
-                        <input type="password" placeholder="password" className="input input-bordered" required />
+                        <input type="email" placeholder="email" name="email" className="input input-bordered" required />
                         <label className="label">
                             <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                         </label>
